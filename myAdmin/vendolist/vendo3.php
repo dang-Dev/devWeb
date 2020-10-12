2927b18684d003007982ee32a7ddd28c3ca51dc3
dang-Dev <70208984+dang-Dev@users.noreply.github.com>
2020-10-12 11:08:54 +0800

    Add files via upload

---
+++ myAdmin/vendolist/vendo3.php
@@ -0,0 +1,135 @@
+    
+    <div class="container-fluid">
+        <h6>&nbsp</h6>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">VENDO3 DASHBOARD</li>
+        </ol>               
+        <div class="row">
+        	<div class="col-lg-3 col-md-6">
+             	<div class="card bg-primary text-white mb-4">
+                    <div class="card-body">
+                        <h1>OFFLINE</h1>
+                        <p>Status:</p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="card bg-warning text-white mb-4">
+                    <div class="card-body">
+                        <h1>123</h1>
+                        <p>Earnings Today:</p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="card bg-dark text-white mb-4">
+                    <div class="card-body">
+                        <h1>RESTART</h1>
+                        <p>Restart Vendo:</p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="card bg-danger text-white mb-4">
+                    <div class="card-body">
+                        <h1>RESET</h1>
+                        <p>Reset Records:</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <div class="card-header bg-purple">
+                <h5 style="color: white;"><i class="fas fa-users"></i> ACTIVE USERS</h5>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Device Name</th>
+                                <th>Username</th>
+                                <th>Ip Address</th>
+                                <th>Mac Address</th>
+                                <th>Pause</th>
+                                <th>Time</th>
+                                <th>Vendo</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                            </tr>
+                            <tr>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <div class="card-header bg-purple">
+                <h5 style="color: white;"><i class="fas fa-users"></i> ALL VENDO USERS</h5>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Device Name</th>
+                                <th>Username</th>
+                                <th>Ip Address</th>
+                                <th>Mac Address</th>
+                                <th>Pause</th>
+                                <th>Time</th>
+                                <th>Vendo</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                            </tr>
+                            <tr>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                                <th>--</th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+	</div>
